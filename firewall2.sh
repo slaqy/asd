8b72cc73864fe89ef8b8b79d4e746f80457c5434
slaqy <cthulhu@slaqy.com>
2024-05-31 13:07:56 +0200

    Add files via upload

--- firewall2.sh
+++ firewall2.sh
@@ -27,22 +27,22 @@ iptables -A INPUT -i enp0s8 -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -i enp0s8 -p icmp --icmp-type echo-reply -j ACCEPT
 
 # Configuración de NAT para dar acceso a Internet a las redes internas
-iptables -t nat -A POSTROUTING -s 192.168.21.0/24 -o enp0s3 -j MASQUERADE
-iptables -t nat -A POSTROUTING -s 192.168.22.0/24 -o enp0s3 -j MASQUERADE
-iptables -t nat -A POSTROUTING -s 192.168.23.0/24 -o enp0s3 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 192.168.30.0/24 -o enp0s3 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 192.168.31.0/24 -o enp0s3 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 192.168.32.0/24 -o enp0s3 -j MASQUERADE
 
 # Proporcionar la IP de debian1 a todo el tráfico hacia la extranet
 iptables -t nat -A POSTROUTING -o enp0s8 -j SNAT --to 192.168.56.2
 
 # Redirección de peticiones al servidor web en debian2 y al servidor ssh en debian5
-iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 8080 -j DNAT --to 192.168.21.2:80
-iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 8443 -j DNAT --to 192.168.21.2:443
-iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 22 -j DNAT --to 192.168.23.1:22
+iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 8080 -j DNAT --to 192.168.30.2:80
+iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 8443 -j DNAT --to 192.168.30.2:443
+iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 31 -j DNAT --to 192.168.32.1:31
 
-# Permitir tráfico hacia debian5 por el puerto 22 (SSH) y hacia debian2 por los puertos 80 (HTTP) y 443 (HTTPS)
-iptables -A FORWARD -d 192.168.23.1 -p tcp --dport 22 -j ACCEPT
-iptables -A FORWARD -d 192.168.21.2 -p tcp --dport 80 -j ACCEPT
-iptables -A FORWARD -d 192.168.21.2 -p tcp --dport 443 -j ACCEPT
+# Permitir tráfico hacia debian5 por el puerto 31 (SSH) y hacia debian2 por los puertos 80 (HTTP) y 443 (HTTPS)
+iptables -A FORWARD -d 192.168.32.1 -p tcp --dport 31 -j ACCEPT
+iptables -A FORWARD -d 192.168.30.2 -p tcp --dport 80 -j ACCEPT
+iptables -A FORWARD -d 192.168.30.2 -p tcp --dport 443 -j ACCEPT
 
 # Permitir todo el tráfico de la intranet
 iptables -A FORWARD -i enp0s3 -j ACCEPT
